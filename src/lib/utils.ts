425b419a3d24615b0f9542af530a54521cfdadbe
Sodiqjonmukhtorov <sodiqjonm16@gmail.com>
2024-08-26 12:06:46 +0500

    first commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,46 @@
+import { type ClassValue, clsx } from "clsx";
+import type { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formatter.format(price);
+};
+
+export function constructMetadata({
+  title = "CaseCobra - custom high-quality phone-cases store",
+  description = "Create custom high-quality phone-cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@Khush_mali_",
+    },
+    icons,
+    metadataBase: new URL("https://casecobra-app-xi.vercel.app"),
+  };
+}
